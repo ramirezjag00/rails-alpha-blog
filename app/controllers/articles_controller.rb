bfa03993a87ea61f806ded71d59008e795fde83f
Andrey <ramirez.jag00@gmail.com>
2018-06-13 13:54:21 +0800

    Added create action template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,18 @@ class ArticlesController < ApplicationController
 
   def create
     # handle the artcle and display it
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
+
+    # instance of an article
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
   end
+
+  # require article as params
+  # allow that article as title and description
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 end
