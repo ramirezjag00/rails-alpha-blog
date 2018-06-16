ffcf9fb42ad14453bb55a6acec3340b3d39b5363
Andrey <ramirez.jag00@gmail.com>
2018-06-17 03:17:28 +0800

    Added controller restrictions for users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,37 +1,25 @@
 class ArticlesController < ApplicationController
-  # before anything else, call set_article method first for whenever these actions are called individually
-  before_action :set_article, only:[:edit, :update, :show, :destroy]
-
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
-
   def new
     @article = Article.new
   end
-
   def edit
-    
   end
-
   def create
-    # debugger
-    
-    # instance of an article
     @article = Article.new(article_params)
-    # will get the first user. (automatically gets the user_id) in database so that each article for now has a User
-    # hardcoded - will delete later after authentication
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created"
-      # article_ is the prefix of article#show rails routes then path is to its path
       redirect_to article_path(@article)
     else
-      # render the form again, this means it went through a validation
       render 'new'
     end
   end
-
   def update
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
@@ -40,26 +28,24 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
-
   def show
   end
-
   def destroy
     @article.destroy
-    flash[:success] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
-
- 
   private
-    # find the article with the parameter :id and save an instance of it with @article
-    def set_article
-      @article = Article.find(params[:id])
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own articles"
+      redirect_to root_path
     end
-
-    # require article as params
-    # allow that article as title and description
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+  end
 end
