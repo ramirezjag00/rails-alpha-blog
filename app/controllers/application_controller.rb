36b3b4bde31588968f7e7f097a78492d4ae8a7f4
Andrey <ramirez.jag00@gmail.com>
2018-06-17 02:50:14 +0800

    Added authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+# Prevent CSRF attacks by raising an exception.
+# For APIs, you may want to use :null_session instead.
+protect_from_forgery with: :exception
+helper_method :current_user, :logged_in?
+def current_user
+  # return current_user, if not, find the user_id in session if there is a user_id
+  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+end
+def logged_in?
+  # bang bang
+  # will return true, converting to boolean
+  !!current_user
+end
+def require_user
+  if !logged_in?
+    flash[:danger] = "You must be logged in to perform that action"
+    redirect_to root_path
+  end
+end
 end
