733be8e0db30dd93bf31d617b7960c7be8b79093
Andrey <ramirez.jag00@gmail.com>
2018-06-16 20:03:48 +0800

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :username, 
+            presence: true, 
+            uniqueness: { case_sensitive: false },
+            length: { in: 3..25 }
+  # http://emailregex.com/
+  # ruby simple version
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, 
+            presence: true,
+            length: { maximum: 105},
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
   has_many :articles
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe User, type: :model do
 
   it { should have_many(:articles) }
 
+  it { should validate_uniqueness_of(:username).case_insensitive }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+
+  it { should validate_length_of(:username).
+      is_at_least(3) }
+
+  it { should validate_length_of(:username).
+      is_at_most(25) }
+
+  it { should validate_length_of(:email).
+      is_at_most(105) }
+
 end
