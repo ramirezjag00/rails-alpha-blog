a9af5d5a84421844b28b366e9b918842b0ad223b
Andrey <ramirez.jag00@gmail.com>
2018-06-16 08:58:10 +0800

    Edit article with validation is now working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,15 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    # find the article with the parameter :id and save an instance of it with @article
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -13,6 +22,16 @@ class ArticlesController < ApplicationController
     else
       # render the form again, this means it went through a validation
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  # this will create routes of articles
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
