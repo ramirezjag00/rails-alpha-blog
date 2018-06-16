f620a6e4a1e0110d3e0002eccc65651f015c98d1
Andrey <ramirez.jag00@gmail.com>
2018-06-16 11:05:38 +0800

    Installed and used shoulda-matchers for Article Model Spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,24 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
 
-  it "should have a title" do
-    article = Article.new(title: nil, description: "Some description")
-    expect(article.save).to eq(false)
-  end
+  it { should validate_presence_of(:title) }
 
-  it "should have a title with length in between 3-50" do
-    article = Article.new(title: "12", description: "Some description")
-    expect(article.save).to eq(false)
-  end
+  it { should validate_length_of(:title).
+      is_at_least(3) }
 
-   it "should have a description" do
-    article = Article.new(title: "1234 678 abcde ", description: nil)
-    expect(article.save).to eq(false)
-  end
+  it { should validate_length_of(:title).
+      is_at_most(50) }
 
-  it "should have a description with length in between 10-300" do
-    article = Article.new(title: "1234 678 abcde ", description: "Some desc")
-    expect(article.save).to eq(false)
-  end
+  it { should validate_presence_of(:description) }
 
+  it { should validate_length_of(:description).
+      is_at_least(10) }
+
+  it { should validate_length_of(:description).
+      is_at_most(300) }
 end
