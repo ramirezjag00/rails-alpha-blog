7a42801c257cf965428213d48b581a053809bb1b
Andrey <ramirez.jag00@gmail.com>
2018-06-21 00:13:15 +0800

    Added edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,11 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
-  
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
-
   def new
     @category = Category.new
   end
-
   def create
     @category = Category.new(category_params)
     if @category.save
@@ -18,11 +15,9 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
-
   def edit
     @category = Category.find(params[:id])
   end
-
   def update
     @category = Category.find(params[:id])
     if @category.update(category_params)
@@ -32,22 +27,18 @@ class CategoriesController < ApplicationController
       render 'edit'
     end
   end
-
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
-
   private
-    def category_params
-      params.require(:category).permit(:name)
+  def category_params
+    params.require(:category).permit(:name)
+  end
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger] = "Only admins can perform that action"
+      redirect_to categories_path
     end
-
-    def require_admin
-      if !logged_in? || (logged_in? and !current_user.admin?)
-        flash[:danger] = "Only admins can perform that action"
-        redirect_to categories_path
-      end
-    end
-
+  end
 end
